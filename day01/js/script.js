110500fd5409d7ce22c4b9b4d581a68cff22b7d7
Carey Kepford <ckepford@gmail.com>
2017-05-15 20:57:31 -0700

    day01

---
+++ day01/js/script.js
@@ -0,0 +1,57 @@
+// ES6 - ECMAScript6
+
+console.log('Sending a message to the console.');
+
+console.info('Sending and information message.');
+
+console.warn('Heres a warning!');
+
+console.error('Danger Will Robinson!');
+
+console.log();
+
+//
+
+/*
+Multi
+Line
+Comments
+*/
+
+var
+
+
+                              freedom
+
+
+
+
+
+
+                                  =
+
+
+
+
+
+
+
+
+                                  "A string of\
+\
+                                   Javascript";
+
+console.log(freedom);
+
+// template string
+
+var javascriptTemplateString = `
+  and
+  type
+  whatever
+  you
+  want
+
+`;
+
+console.log(javascriptTemplateString);
